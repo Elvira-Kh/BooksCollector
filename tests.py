f39108d8f6ec1ae2b651275920f02bd786bba253
Elvira <138506199+Elvira-Kh@users.noreply.github.com>
2024-02-09 12:17:55 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -3,15 +3,9 @@ import pytest
 
 from main import BooksCollector
 
-
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
+
     def test_add_new_book_add_two_books(self):
         # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
@@ -24,10 +18,7 @@ class TestBooksCollector:
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_genre()) == 2
 
-    # напиши свои тесты ниже
-
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
-
+    
     #проверяем возможность добавить книгу
     def test_add_new_book(self):
         collector = BooksCollector()
